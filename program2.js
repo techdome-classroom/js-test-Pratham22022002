6ca94769011c2333bd9589be0e6980e647f34fcb
Pratham22022002 <162552596+Pratham22022002@users.noreply.github.com>
2024-05-09 12:39:21 +0530

    Update program2.js

--- program2.js
+++ program2.js
@@ -1,7 +1,23 @@
-const decodeTheRing = function (s, p) {
+const decodeTheRing = function (m,k) {
 
-    // write your code here
+    def decode(mesaage, key):
+    if len(message) != len(key):
+        return False
+    
+    for m, k in zip(message, key):
+        if k == '?' or k == m:
+            continue
+        elif k == '*':
+            continue
+        else:
+            return False
+    
+    return True
 
-  };
+print(decode("aa", "a"))      
+print(decode("aa", "*"))      
+print(decode("cb", "?a"))     
+    
+};
   
   module.exports = decodeTheRing;
